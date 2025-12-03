29c33a0983526ac9a5c534c80a5ae98a85957062
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 14:36:05 +1100

    feat (obstacle): Added skeleton obstacle superclass

---
+++ src/entities/obstacles/obstacle.py
@@ -0,0 +1,16 @@
+class Obstacle:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        
+        self.speed = 0
+
+        self.width = 0
+        self.height = 0
+
+        def update(self):
+            pass
+
+        def draw(self, screen):
+            pass
+
