e5e659f59ea8ac635e7488a7938d14a8a0de52ee
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 16:41:07 +1100

    refactor (Obstacle): Moved loading and scaling of cactus image from cactus class into factory. Added create_obstacle logic in factory

--- src/entities/obstacles/ObstacleFactory.py
+++ src/entities/obstacles/ObstacleFactory.py
@@ -1,18 +1,30 @@
 from .cactus import Cactus
 
 import random
+import pygame
 
 class ObstacleFactory:
     def __init__(self):
+
+        self.obstacleList = ["Cactus", "Bird"]
+
+        # CACTUS
         self.CACTUS_X_LEVEL = 1000
         self.CACTUS_Y_LEVEL = 200
 
-        self.cactus_types = ["TallShortCactus", "TallMediumCactus", "TallLongCactus", "ShortShortCactus", "ShortMediumCactus", "ShortLongCactus"]
+        self.cactus_types = ["LargeShortCactus", "LargeMediumCactus", "LargeLongCactus", "SmallShortCactus", "SmallMediumCactus", "SmallLongCactus"]
         self.cactus_weights = [20, 12, 8, 30, 20, 10]
 
-        self.obstacle_types = ["Cactus"]
-        self.spawn_interval = 0
-        self.last_spawntime = 0
+        # Cactus images
+        self.cactus_scale_factor = 0.5
+        self.cactusImages = {
+            "LargeShortCactus": self.scale_cactus("assets/cactus/LargeCactus1.png"),
+            "LargeMediumCactus": self.scale_cactus("assets/cactus/LargeCactus2.png"),
+            "LargeLongCactus": self.scale_cactus("assets/cactus/LargeCactus3.png"),
+            "SmallShortCactus": self.scale_cactus("assets/cactus/SmallCactus1.png"),
+            "SmallMediumCactus": self.scale_cactus("assets/cactus/SmallCactus2.png"),
+            "SmallLongCactus": self.scale_cactus("assets/cactus/SmallCactus3.png"),
+        }
 
 
     def create_obstacle(self, speed):
@@ -20,13 +32,21 @@ class ObstacleFactory:
         # If cactus, run define_cactus_obstacle to determine which cactus to spawn
         # Spawning logic.
 
-        return Cactus(self.CACTUS_X_LEVEL, self.CACTUS_Y_LEVEL, speed)
+        selectedObstacle = self.define_cactus_obstacle()
+        image = self.cactusImages[selectedObstacle]
+
+        return Cactus(self.CACTUS_X_LEVEL, self.CACTUS_Y_LEVEL, speed, image)
     
     
     def define_cactus_obstacle(self):
         # Random calculation to determine which cactus to spawn.
-        chosen_cactus = random.choices(self.cactus_types, weights=self.weights, k=1)[0]
+        chosen_cactus = random.choices(self.cactus_types, weights=self.cactus_weights, k=1)[0]
 
         return chosen_cactus
     
     
+    def scale_cactus(self, path):
+        img = pygame.image.load(path)
+        w = int(img.get_width() * self.cactus_scale_factor)
+        h = int(img.get_height() * self.cactus_scale_factor)
+        return pygame.transform.scale(img, (w, h))
--- src/entities/obstacles/cactus.py
+++ src/entities/obstacles/cactus.py
@@ -5,34 +5,10 @@ import os
 import random
 
 class Cactus(Obstacle):
-    def __init__(self, x, y, speed):
+    def __init__(self, x, y, speed, image):
 
-        self.image = self.image_preprocessing()
+        self.image = image
 
         super().__init__(x, y, speed, self.image)
 
 
-    def image_preprocessing(self):
-        # Cactus imaging
-        self.scale_factor = 0.5
-
-        self.LargeCactus1 = pygame.image.load(os.path.join("assets", "cactus", "LargeCactus1.png"))
-        self.LargeCactus2 = pygame.image.load(os.path.join("assets", "cactus", "LargeCactus2.png"))
-        self.LargeCactus3 = pygame.image.load(os.path.join("assets", "cactus", "LargeCactus3.png"))
-
-        self.SmallCactus1 = pygame.image.load(os.path.join("assets", "cactus", "SmallCactus1.png"))
-        self.SmallCactus2 = pygame.image.load(os.path.join("assets", "cactus", "SmallCactus2.png"))
-        self.SmallCactus3 = pygame.image.load(os.path.join("assets", "cactus", "SmallCactus3.png"))
-
-        temp_cactusList = [self.LargeCactus1, self.LargeCactus2, self.LargeCactus3, self.SmallCactus1, self.SmallCactus2, self.SmallCactus3]
-        cactusList = []
-
-        for cactus in temp_cactusList:
-            cactus_new_width = int(cactus.get_width() * self.scale_factor)
-            cactus_new_height = int(cactus.get_height() * self.scale_factor)
-
-            cactusList.append(pygame.transform.scale(cactus, (cactus_new_width, cactus_new_height)))
-
-        random_int = random.randint(0,5)
-
-        return cactusList[random_int]
