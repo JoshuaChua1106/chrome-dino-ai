bd00aef38a19a7ce9e07b677c1c24bc8fedd8241
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 15:53:44 +1100

    feat (ObstacleSpawner): Added cactus selection method

--- src/entities/obstacles/ObstacleSpawner.py
+++ src/entities/obstacles/ObstacleSpawner.py
@@ -1,5 +1,6 @@
 from .cactus import Cactus
 
+import random
 
 class ObstacleSpawner:
     def __init__(self):
@@ -7,13 +8,27 @@ class ObstacleSpawner:
         self.CACTUS_Y_LEVEL = 200
 
         self.cactus_types = ["TallShortCactus", "TallMediumCactus", "TallLongCactus", "ShortShortCactus", "ShortMediumCactus", "ShortLongCactus"]
+        self.cactus_weights = [20, 12, 8, 30, 20, 10]
 
         self.obstacle_types = ["Cactus"]
         self.spawn_interval = 0
         self.last_spawntime = 0
 
+
+    def create_obstacle(self, speed):
+        # Random to determine if cactus or bird (0.75 cactus, 0.25 bird)
+        # If cactus, run define_cactus_obstacle to determine which cactus to spawn
+        # Spawning logic.
+
+        return Cactus(self.CACTUS_X_LEVEL, self.CACTUS_Y_LEVEL, speed)
+    
+
     def should_spawn(self, current_time):
         return current_time - self.last_spawntime > self.spawn_interval
 
-    def create_obstacle(self, speed):
-        return Cactus(self.CACTUS_X_LEVEL, self.CACTUS_Y_LEVEL, speed)
+    
+    def define_cactus_obstacle(self):
+        # Random calculation to determine which cactus to spawn.
+        chosen_cactus = random.choices(self.cactus_types, weights=self.weights, k=1)[0]
+
+        return chosen_cactus
