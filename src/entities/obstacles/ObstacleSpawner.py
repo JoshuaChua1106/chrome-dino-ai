7e5ac2c7c892ccfcbcb394781f4651b7578fd3c1
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 16:16:25 +1100

    feat (ObstacleSpawner): Added timing logic to when to spawn an obstacle

--- src/entities/obstacles/ObstacleSpawner.py
+++ src/entities/obstacles/ObstacleSpawner.py
@@ -1,16 +1,19 @@
 from .ObstacleFactory import ObstacleFactory
 
 import random
+import pygame
 
 class ObstacleSpawner:
-    def __init__(self, frame_count, speed):
+    def __init__(self, speed):
         self.factory = ObstacleFactory()
         self.spawn_timer = 0
 
         # Spawning information
-        self.spawn_interval = 0
         self.last_spawntime = 0
-        self.frame_count = frame_count
+        self.spawn_interval = 1500 #(ms)
+
+        self.current_time = pygame.time.get_ticks()
+
 
         # Obstacle information
         self.speed = speed
@@ -18,8 +21,11 @@ class ObstacleSpawner:
 
     def update(self):
         if self.should_spawn:
+            self.last_spawn_time = self.current_time
             return self.factory.createObstacle(self.speed)
 
-    def should_spawn(self, current_time):
-        return current_time - self.last_spawntime > self.spawn_interval
-
+    def should_spawn(self):
+        if self.current_time - self.last_spawn_time >= self.spawn_interval:
+            return True
+        else:
+            return False
