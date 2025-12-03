456e30711a931aca57d3798e3b7cf9572dd32363
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 15:33:15 +1100

    feat (obstacle): Created obstacle factory to instantiate cactus

---
+++ src/entities/obstacles/ObstacleFactory.py
@@ -0,0 +1,19 @@
+from .cactus import Cactus
+
+
+class ObstacleFactory:
+    def __init__(self):
+        self.CACTUS_X_LEVEL = 1000
+        self.CACTUS_Y_LEVEL = 200
+
+        self.cactus_types = ["TallShortCactus", "TallMediumCactus", "TallLongCactus", "ShortShortCactus", "ShortMediumCactus", "ShortLongCactus"]
+
+        self.obstacle_types = ["Cactus"]
+        self.spawn_interval = 0
+        self.last_spawntime = 0
+
+    def should_spawn(self, current_time):
+        return current_time - self.last_spawntime > self.spawn_interval
+
+    def create_obstacle(self, speed):
+        return Cactus(self.CACTUS_X_LEVEL, self.CACTUS_Y_LEVEL, speed)
