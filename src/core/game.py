f4776890acd6d7087f651056d438a0cf3a9b6efa
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-03 12:15:25 +1100

    feat (game): Added a frame rate counter to game class

--- src/core/game.py
+++ src/core/game.py
@@ -3,10 +3,16 @@ import pygame
 
 class Game:
     def __init__(self, screen):
+
+        # Initializing Pygame screen
         self.screen = screen
         self.clock = pygame.time.Clock()
+
+        # Game states
         self.running = True
 
+        # Counting frames
+        self.frame_count = 0
 
         # Instantiate classes
         self.dino = Dino()
@@ -14,10 +20,16 @@ class Game:
 
     def run(self):
         while self.running:
+            # Game logic
             self.draw()
             self.handle_events()
             self.update()
+
+            # Control frame-rate
             self.clock.tick(60)
+            self.frame_count += 1
+
+            # Updated Pygame
             pygame.display.flip()
 
 
